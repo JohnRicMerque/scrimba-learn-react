0b28267b412506475b1706b3e9b400dcd4d6adad
John Ric Merque <johnricmer@gmail.com>
2023-03-15 18:11:56 +0800

    added preface

---
+++ airbnb-clone/components/Preface.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Preface() {
+    return (
+        <section className="preface">
+            <img src="../images/photo-grid.png" className="preface--photo"/>
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
--- airbnb-clone/index.jsx
+++ airbnb-clone/index.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import NavBar from './components/NavBar'
+import Preface from './components/Preface'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NavBar />
+    <Preface />
     <App />
   </React.StrictMode>,
 )
