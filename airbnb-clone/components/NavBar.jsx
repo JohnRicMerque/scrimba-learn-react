41e02a728c3f3ce6db106f019f2e2ffbf5467d71
John Ric Merque <johnricmer@gmail.com>
2023-03-15 17:30:43 +0800

    added NavBar component

---
+++ airbnb-clone/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NavBar(){
+    return (
+        <nav>
+            <img src="./images/airbnb-logo.png" className="nav--logo" />
+        </nav>
+    )
+}
