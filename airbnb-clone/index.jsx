eb8eb2bb67feed52726bbd030f58060c87117596
John Ric Merque <johnricmer@gmail.com>
2023-03-16 21:09:52 +0800

    added card component

--- airbnb-clone/index.jsx
+++ airbnb-clone/index.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import NavBar from './components/NavBar'
 import Preface from './components/Preface'
+import Card from './components/Card'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NavBar />
     <Preface />
+    <Card img="../images/katie-zaferes.png" rating="5.0" reviewCount={6} country="Philippines" title="Life Lessons with Katie Zaferes" price={136}/>
     <App />
   </React.StrictMode>,
 )
