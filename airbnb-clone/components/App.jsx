bef11d0f29898de874c84865cf701e2b8026aab2
John Ric Merque <johnricmer@gmail.com>
2023-03-16 22:30:21 +0800

    code refactor/added database

--- airbnb-clone/components/App.jsx
+++ airbnb-clone/components/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import NavBar from './NavBar'
+import Preface from './Preface'
+import Card from './Card'
+import Data from './data'
 
 export default function App() {
+
+  const cards = Data.map(item => {
+    return <Card
+    img={item.coverImg}
+    rating={item.stats.rating}
+    reviewCount={item.stats.reviewCount}
+    location={item.location}
+    title={item.title}
+    price={item.price}
+    />
+  })
+
   return (
     <div className="App">
+      <NavBar />
+      <Preface />
+      {cards}
     </div>
   )
 }
